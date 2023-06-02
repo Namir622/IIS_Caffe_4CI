9c13ce2151b7dffd47eb610bb860ac414c0b0d99
Namir622 <marcomac2005@gmail.com>
2023-06-02 11:46:33 +0200

    s

--- JAVA/Menu_grafico/src/Jframe.java
+++ JAVA/Menu_grafico/src/Jframe.java
@@ -1,23 +1,68 @@
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 
-public class Jframe extends JFrame {
-    
+public class Jframe extends JFrame implements MenuListener{
+
+    static JMenu menu;
+
+    static JMenuItem T1;
+    static JMenuItem T2;
+    static JMenuItem T3;
     Jframe(){
         this.setTitle("bloc Notes");
         this.setBounds(300, 300, 600, 400);
         this.setResizable(false);
         this.setDefaultCloseOperation(Jframe.EXIT_ON_CLOSE);
 
-        
+
         Pannello coso = new Pannello();
         
+        JMenuBar bar = new JMenuBar();
+        menu = new JMenu("Lista");
+        bar.add(menu);
+        T1 = new JMenuItem("File 1", ABORT);
+        T2 = new JMenuItem("File 2", ABORT);
+        T3 = new JMenuItem("File 3", ABORT);
+        menu.add(T1);menu.add(T2);menu.add(T3);
+
+        menu.addMenuListener(this);
+
+        this.setJMenuBar(bar);
+
+
         this.add(coso);
-        
         this.setVisible(true);
     }//costruttore
 
     public static void main(String[] args) {
-        new Jframe();
+        new Jframe(); 
     }//main
+
+
+
+
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+        if (e.getSource() == T1) 
+        {
+            
+        } else {
+            
+        }
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'menuDeselected'");
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'menuCanceled'");
+    }
      
 }//class
--- JAVA/Menu_grafico/src/Pannello.java
+++ JAVA/Menu_grafico/src/Pannello.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.JMenuBar;
 import java.awt.*;
 
 
@@ -10,22 +9,7 @@ public class Pannello extends JPanel {
     Pannello(){
          this.setBackground(Color.red);
          this.setLayout(new  BorderLayout());
-
-
-         JMenuBar bar = new JMenuBar();
-         JMenu menu = new JMenu("Lista");
-         bar.add(menu);
-         JMenuItem T1 = new JMenuItem("File 1", ABORT);
-         JMenuItem T2 = new JMenuItem("File 2", ABORT);
-         JMenuItem T3 = new JMenuItem("File 3", ABORT);
-         menu.add(T1);menu.add(T2);menu.add(T3);
-        
-
-         //Pannello menu
-         JPanel nord = new JPanel();
-         nord.setBackground(Color.yellow);
-         nord.setPreferredSize(new Dimension(500, 50));
-         nord.add(bar);
+    
          
          //Pannello file
          JPanel file = new JPanel();
@@ -41,11 +25,9 @@ public class Pannello extends JPanel {
 
          //TextArea
          Area = new JTextArea();
+         Area.setPreferredSize(new Dimension(400, 440));
 
 
-
-
-        this.add(nord, BorderLayout.NORTH);
         this.add(file, BorderLayout.WEST);
         this.add(bottoni, BorderLayout.SOUTH);
         this.add(Area, BorderLayout.CENTER);
